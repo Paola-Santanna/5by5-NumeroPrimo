d1e11237b49626915ac1e14c557985d637f5c61c
Paola-Santanna <paolinhasj@gmail.com>
2024-04-19 15:35:23 -0300

    Foi definido as partes do programa, as variáveis e a requisição básica de informação do usuário.

--- Número Primo/Program.cs
+++ Número Primo/Program.cs
@@ -1,1 +1,17 @@
 ﻿//Faça um rpograma que leia um número (do tipo que você preferir). Informe se este número é primo;
+
+//Início do programa
+Console.WriteLine("--- Verificação de Número Primo ---\n");
+
+//Criação de variáveis
+float numero, num_aux;
+
+//Entrada de informação
+Console.Write("Insira um número: ");
+numero = float.Parse(Console.ReadLine());
+
+//Operações
+
+
+//Final do programa
+Console.ReadKey();
