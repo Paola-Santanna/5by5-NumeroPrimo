0eae70c633a98a9a310735a018e233ee5378ff49
Paola-Santanna <paolinhasj@gmail.com>
2024-04-21 22:00:32 -0300

    #Checagem de Número Primo
    Foram usadas as estruturas de repetição e condição.

--- Número Primo/Program.cs
+++ Número Primo/Program.cs
@@ -4,14 +4,59 @@
 Console.WriteLine("--- Verificação de Número Primo ---\n");
 
 //Criação de variáveis
-float numero, num_aux;
+int numero, num_aux;
 
-//Entrada de informação
+//Entrada de dados e Operações
 Console.Write("Insira um número: ");
-numero = float.Parse(Console.ReadLine());
+numero = int.Parse(Console.ReadLine());
+num_aux = numero;
 
-//Operações
-
+for (int i = 1; i <= num_aux; i++)
+{
+    if (num_aux / i == num_aux && num_aux / i != 1)
+    {
+        if (num_aux % (i + 1) == 0)
+        {
+            Console.WriteLine("Não é primo");
+            i = num_aux;
+        }
+        else
+        {
+            if (num_aux % (i + 2) == 0 && num_aux != 3)
+            {
+                Console.WriteLine("Não é primo");
+                i = num_aux;
+            }
+            else
+            {
+                if (num_aux % (i + 4) == 0 && num_aux != 5)
+                {
+                    Console.WriteLine("Não é primo");
+                    i = num_aux;
+                }
+                else
+                {
+                    if (num_aux % (i + 6) == 0 && num_aux != 7)
+                    {
+                        Console.WriteLine("Não é primo");
+                        i = num_aux;
+                    }
+                    else
+                    {
+                        Console.WriteLine("É primo");
+                        i = num_aux;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("É primo");
+        Console.WriteLine(i);
+        i = num_aux;
+    }
+}
 
 //Final do programa
 Console.ReadKey();
