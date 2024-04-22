47b48f746815acd0c795153627310e02cbdc2bb4
Paola-Santanna <paolinhasj@gmail.com>
2024-04-22 09:33:59 -0300

    #Correção em Aula
    Os números primos são a partir de 1.

--- Número Primo/Program.cs
+++ Número Primo/Program.cs
@@ -3,7 +3,7 @@
 int numero, num_aux;
 
 //Entrada de dados e Operações
-Console.WriteLine("Somente digite números POSITIVOS e DIFERENTES de ZERO\n");
+Console.WriteLine("Somente digite números MAIORES que UM\n");
 
 do
 {
@@ -11,19 +11,19 @@ do
     numero = int.Parse(Console.ReadLine());
     num_aux = numero;
 
-    if (num_aux < 0 || num_aux == 0)
+    if (num_aux <= 1)
     {
         Console.WriteLine("\nResposta Inválida!");
         Console.WriteLine("Digite novamente!\n");
     }
 
-} while (numero < 0 || num_aux == 0);
+} while (numero <= 1);
 
 for (int i = 1; i <= num_aux; i++)
 {
     if (num_aux / i == num_aux && num_aux / i != 1)
     {
-        if (num_aux % (i + 1) == 0)
+        if (num_aux % (i + 1) == 0 && num_aux != 2)
         {
             Console.WriteLine("Não é primo");
             i = num_aux;
@@ -57,11 +57,6 @@ for (int i = 1; i <= num_aux; i++)
                 }
             }
         }
-    }
-    else
-    {
-        Console.WriteLine("É primo");
-        i = num_aux;
     }
 }
 //Final do programa
